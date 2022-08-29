f335dcd737e4e25e2c752196fc97d583715bed40
chrissiemhrk <chrissiemhrk@gmail.com>
2022-08-29 12:06:40 +0200

    make window transparent

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="app">my first electron app. woohoo!!</div>;
 };
 
 export default App;
